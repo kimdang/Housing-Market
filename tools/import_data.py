85c2f36b0ff1fdb96e801715c050806e276256ed
Kim Dang <nkdang0795@gmail.com>
2019-04-09 20:34:35 -0700

    convert datetime and convert dollar to int

--- tools/import_data.py
+++ tools/import_data.py
@@ -8,6 +8,30 @@
 import pandas as pd
 import sqlalchemy as sqlal
 
+def convert_dollar_to_int ():
+    s=sanjose['Single Family Home'].str.replace(",", '')
+    a=s.str.lstrip('$')
+    c=a.astype('int')
+    sanjose['Single Family Home'] = c
+
+def convert_to_datetime ():
+    start = datetime.datetime(2000,1,1)
+    mylist = []
+    ngan = start
+    for item in sanjose['Date']:
+        mylist.append(ngan)
+        ngan = add_months(start,1)
+        start = ngan
+
+
+def add_months(sourcedate, months):
+    month = sourcedate.month - 1 + months
+    year = sourcedate.year + month // 12
+    month = month % 12 + 1
+    day = min(sourcedate.day, calendar.monthrange(year,month)[1])
+    return datetime.date(year, month, day)
+
+
 # path to the data file 
 filename = ""
 city = pd.read_csv(filename)
@@ -21,5 +45,5 @@ host = ""
 database = ""
 engine = sqlal.create_engine("mysql://" + username + ":" + password + "@" + host + "/" + database)
 con = engine.connect()
-city.to_sql(name='table_name',con=con,if_exists='append')
+city.to_sql(name='table_name',con=con,if_exists='replace')
 con.close()

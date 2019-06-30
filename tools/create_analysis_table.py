259bbba51dda9f4121eacfc092101b616cf3d749
Kim Dang <nkdang0795@gmail.com>
2019-06-30 16:07:30 -0700

    fix analysis table by adding foreign key to location_id column, url would be able display image instead of download, add title and yaxid label to image

--- tools/create_analysis_table.py
+++ tools/create_analysis_table.py
@@ -1,6 +1,10 @@
+
+
+
 import credential
 import pymysql.cursors
 
+# ESTABLISH CONNECTION TO DATABASE
 HOST = credential.credentials['host']
 USER = credential.credentials['username']
 PASSWORD = credential.credentials['password']
@@ -13,9 +17,11 @@ conn = pymysql.connect(host=HOST,
                        charset='utf8mb4', 
                        cursorclass=pymysql.cursors.DictCursor)
 
+# CREATE TABLE analysis IN DATABASE
 with conn.cursor() as cursor:
-    query = "CREATE TABLE analysis (id INT, url VARCHAR(255), location_id INT)"
+    query = "CREATE TABLE analysis (ind INT AUTO_INCREMENT PRIMARY KEY, url VARCHAR(255), location_id INT, FOREIGN KEY (location_id) REFERENCES location_id(ind) ON UPDATE CASCADE)"
     cursor.execute(query)
     print("Table created.")
 
+# CLOSE CONNECTION
 conn.close()

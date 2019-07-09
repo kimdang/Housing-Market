a09d2c4750b78d8bab82dd9b6e325f36778713b1
Kim Dang <nkdang0795@gmail.com>
2019-07-08 18:23:51 -0700

    create a table for each city and import estimated home values into each corresponding table/city

--- tools/import_homevalue_into_db.py
+++ tools/import_homevalue_into_db.py
@@ -1,33 +1,28 @@
+# create table for each city using dataset homevalue
+
+
 import pandas as pd
 import execute_mysql 
-import datetime
+import my_tools
 
-
-def prep_data (raw_df, ind):
-    city = raw_df.loc[ind,:]
-    city = city.T
-    city = city.drop(city.index[0:7])
-    city = city.dropna().astype(int)
-    city = city.to_frame()
-    count = city[ind].count()
-    date_list = []
-    for i in range(count):
-        date = datetime.datetime.strptime(city.index[i], "%Y-%m")
-        date_list.append(date)
-    new_df = pd.DataFrame(city[ind], index=date_list)
-    return new_df
 
 
 homevalue = pd.read_csv('zhvi_allhomes.csv')
+city_count = homevalue['RegionName'].count()
 
 
-for i in range(3):
-    city_to_db = prep_data(homevalue, i)
+
+for i in range(2): # <----- change to city_count
+    city_to_db = my_tools.prep_data(homevalue, i, drop_column=7)
     city_to_db.columns = ['value']
     location_id = i+1
+    # location_id table was created using dataset homevalue
+    # location_id start at 1, while index of dataset homevalue start at 0, therefore location_id = index + 1
 
     table_query = "CREATE TABLE data_%s (ind INT AUTO_INCREMENT PRIMARY KEY, dt DATETIME NOT NULL, homevalue INT)" %(location_id)
     execute_mysql.run_query(table_query)
+    # each table is named data_<location_id>
+
 
     row_count = city_to_db['value'].count()
     for j in range(row_count):

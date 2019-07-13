6a8f1ca687b15c32969e09758e13186607a63112
Kim Dang <nkdang0795@gmail.com>
2019-07-13 10:36:14 -0700

    use listing dataset to create location table instead of homevalue dataset

--- tools/create_location_table.py
+++ tools/create_location_table.py
@@ -1,40 +1,16 @@
-
-
-import pymysql.cursors
-import credential
 import pandas as pd
+import translate_state_name
+import my_tools
+import execute_mysql
 
-# ESTABLISH CONNECTION TO DATABASE 
-HOST = credential.credentials['host']
-USER = credential.credentials['username']
-PASSWORD = credential.credentials['password']
-DB = credential.credentials['name']
 
-conn = pymysql.connect(host=HOST, 
-                       user=USER, 
-                       password=PASSWORD, 
-                       db=DB, 
-                       charset='utf8mb4', 
-                       cursorclass=pymysql.cursors.DictCursor)
+listing = pd.read_csv('listing_prices.csv')
+new_state = pd.DataFrame(listing['StateName'].map(translate_state_name.state_dictionary))
 
-# PULL RAW DATA zillow_by_city FROM DATABASE
-zillow_by_city = pd.read_sql("SELECT * FROM zillow_by_city", con=conn)
-location = zillow_by_city[['index', 'RegionName', 'State']]
+table_query = "CREATE TABLE location (id INT AUTO_INCREMENT PRIMARY KEY, city VARCHAR(255), state VARCHAR(255)"
+execute_mysql.run_query(table_query)
 
-# CREATE TABLE location_id IN DATABASE
-with conn.cursor() as cursor:
-    table_query = "CREATE TABLE location_id (ind INT AUTO_INCREMENT PRIMARY KEY, city VARCHAR(255), state VARCHAR(255))"
-    cursor.execute(table_query)
-    print('Table created.')
 
-for i in range(location['index'].count()):
-    with conn.cursor() as cursor:
-        insert_query = "INSERT INTO location_id (city, state) VALUES (%s, %s)"
-        val = (location['RegionName'][i], location['State'][i])
-        cursor.execute(insert_query, val)
-        conn.commit()    
-        print('Insertion completed.')
-
-# Close connection
-conn.close()
-
+for i in range(listing['RegionName'].count()):
+    insert_query = "INSERT INTO location (city, state) VALUES ('%s', '%s')" %(listing['RegionName'][i], new_state['StateName'][i])
+    execute_mysql.run_query(insert_query)

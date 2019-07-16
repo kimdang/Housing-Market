9585b82d6b6b9abb617feeb9e62178b0ef13d835
Kim Dang <nkdang0795@gmail.com>
2019-07-15 17:16:37 -0700

    add a feature that drop duplicates where multiple datasets exist for 1 city

--- tools/create_location_table.py
+++ tools/create_location_table.py
@@ -1,16 +1,24 @@
 import pandas as pd
 import translate_state_name
-import my_tools
 import execute_mysql
 
 
+
+# location ID is created from listing dataset 
+# state name will be abbreviated
+# there are cases where multiple datasets exist for 1 city
 listing = pd.read_csv('listing_prices.csv')
+listing.drop_duplicates(['RegionName', 'StateName'], keep=False)
 new_state = pd.DataFrame(listing['StateName'].map(translate_state_name.state_dictionary))
+
+
 
 table_query = "CREATE TABLE location (id INT AUTO_INCREMENT PRIMARY KEY, city VARCHAR(255), state VARCHAR(255))"
 execute_mysql.run_query(table_query)
 
 
+
 for i in range(listing['RegionName'].count()):
     insert_query = "INSERT INTO location (city, state) VALUES ('%s', '%s')" %(listing['RegionName'][i], new_state['StateName'][i])
     execute_mysql.run_query(insert_query)
+

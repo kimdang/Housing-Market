f95109b27158ed1d78f9fc725b50d8e885c259e3
Kim Dang <nkdang0795@gmail.com>
2019-07-23 22:45:40 -0700

    fix connection issue with django and pymysql engine

--- tools/execute_mysql.py
+++ tools/execute_mysql.py
@@ -6,14 +6,16 @@ USER = credential.credentials['username']
 PASSWORD = credential.credentials['password']
 DB = credential.credentials['name']
 
-conn = pymysql.connect(host=HOST, 
+
+def run_query (query, fetch=False, fetch_option='fetchone'):
+
+    conn = pymysql.connect(host=HOST, 
                        user=USER, 
                        password=PASSWORD, 
                        db=DB, 
                        charset='utf8mb4', 
                        cursorclass=pymysql.cursors.DictCursor)
-
-def run_query (query, fetch=False, fetch_option='fetchone'):
+                       
     print(query)
     with conn.cursor() as cursor:
         cursor.execute(query)
